3f9eebb6035685ff836e4a9949131301e1d93f30
Carlo Munguia <carlomunguia@icloud.com>
2021-01-31 01:20:45 -0600

    guessing game: complete! added a main loop, enhanced error structure

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -6,14 +7,29 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Small? Yes."),
+            Ordering::Greater => println!("Too big? Is that a thing?"),
+            Ordering::Equal => {
+                println!("YOU WIN");
+                break;
+            }
+        }
+    }
 }
